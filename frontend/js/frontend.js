250df526f79dd173c99984b8c7bb448f161eb3b3
Kush Shah <kshah2@wpi.edu>
2025-10-04 14:33:07 -0700

    show randomResult on button press

--- frontend/js/frontend.js
+++ frontend/js/frontend.js
@@ -1,1 +1,8 @@
-console.log('Frontend JS loaded successfully.');
+console.log("Frontend JS loaded successfully.");
+
+//eventListener for random button
+document.getElementById("generateSuggestionBtn").addEventListener("click", function () {
+    console.log("Generate Suggestion button clicked.");
+    let result = document.getElementById("suggestionResult");
+    result.hidden = false;
+});
